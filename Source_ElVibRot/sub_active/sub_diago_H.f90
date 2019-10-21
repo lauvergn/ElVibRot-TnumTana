1dc98c311f4777165dd4806428dbf6afff03d236
AChen <AChen>
2019-10-21 02:13:44 +0200

    Source_ElVibRot/sub_active/sub_diago_H.f90 done

--- Source_ElVibRot/sub_active/sub_diago_H.f90
+++ Source_ElVibRot/sub_active/sub_diago_H.f90
@@ -25,10 +25,13 @@
 !        - Somme subroutines of SHTOOLS written by Mark A. Wieczorek under BSD license
 !             http://shtools.ipgp.fr
 !===========================================================================
-!===========================================================================
+!=======================================================================================
       SUBROUTINE sub_diago_H(H,E,Vec,n,sym)
       USE mod_system
       USE mod_Constant, ONLY: get_Conv_au_TO_unit
+#IF(run_MPI)      
+      USE mod_MPI
+#ENDIF
       IMPLICIT NONE
 
 !------ active Matrix H Vec E ------------------------------------
@@ -38,7 +41,6 @@
       real (kind=Rkind) :: Vec(n,n)
       real (kind=Rkind) :: E(n)
       logical           :: sym
-
 
 !----- divers --------------------------------------------------------
       real (kind=Rkind) :: auTOcm_inv
@@ -60,24 +62,23 @@
          write(out_unitp,*) 'n',n
          CALL flush_perso(out_unitp)
        END IF
-!-----------------------------------------------------------
-!=====================================================================
-!
+!---------------------------------------------------------------------------------------
 !       H matrix diagonalisation
-!
-!=====================================================================
+!---------------------------------------------------------------------------------------
   auTOcm_inv = get_Conv_au_TO_unit('E','cm-1')
 
-  IF (residual) THEN
-    CALL alloc_NParray(Hsave,(/ n,n /),'Hsave',name_sub)
-    CALL alloc_NParray(r,(/ n /),'r',name_sub)
-    Hsave(:,:) = H(:,:)
-  END IF
+  IF(MPI_id==0) THEN
+    IF (residual) THEN
+      CALL alloc_NParray(Hsave,(/ n,n /),'Hsave',name_sub)
+      CALL alloc_NParray(r,(/ n /),'r',name_sub)
+      Hsave(:,:) = H(:,:)
+    END IF
+  ENDIF
 
   IF (.NOT. sym) THEN
-    CALL diagonalization(H,E,Vec,n,4,1,.TRUE.)
+    IF(MPI_id==0) CALL diagonalization(H,E,Vec,n,4,1,.TRUE.)
   ELSE
-    CALL diagonalization(H,E,Vec,n,3,1,.TRUE.)
+    IF(MPI_id==0) CALL diagonalization(H,E,Vec,n,3,1,.TRUE.)
   END IF
 
   IF (debug) THEN
@@ -85,39 +86,36 @@
     write(out_unitp,*) 'OpMin,OpMax (cm-1): ',minval(E)*auTOcm_inv,maxval(E)*auTOcm_inv
   END IF
 
-
-  DO i=1,n
-    A = maxval(Vec(:,i))
-    B = minval(Vec(:,i))
-    IF (abs(A) < abs(B)) Vec(:,i) = -Vec(:,i)
-  END DO
-
-  IF (residual) THEN
+  IF(MPI_id==0) THEN
     DO i=1,n
-      r(:) = matmul(Hsave,Vec(:,i))-E(i)*Vec(:,i)
-      write(out_unitp,*) 'residual (cm-1)',i,sqrt(dot_product(r,r))*&
-                                                        auTOcm_inv
-      write(out_unitp,*) 'residual (au)',i,sqrt(dot_product(r,r))
-      write(out_unitp,*) 'err (cm-1)',i,dot_product(Vec(:,i),r)*    &
-                                                         auTOcm_inv
-      write(out_unitp,*) 'err (au)',i,dot_product(Vec(:,i),r)
+      A = maxval(Vec(:,i))
+      B = minval(Vec(:,i))
+      IF (abs(A) < abs(B)) Vec(:,i) = -Vec(:,i)
     END DO
-    CALL dealloc_NParray(Hsave,'Hsave',name_sub)
-    CALL dealloc_NParray(r,'r',name_sub)
-  END IF
+  ENDIF
+  
+  IF(MPI_id==0) THEN
+    IF (residual) THEN
+      DO i=1,n
+        r(:) = matmul(Hsave,Vec(:,i))-E(i)*Vec(:,i)
+        write(out_unitp,*) 'residual (cm-1)',i,sqrt(dot_product(r,r))*&
+                                                         auTOcm_inv
+        write(out_unitp,*) 'residual (au)',i,sqrt(dot_product(r,r))
+        write(out_unitp,*) 'err (cm-1)',i,dot_product(Vec(:,i),r)*    &
+                                                         auTOcm_inv
+        write(out_unitp,*) 'err (au)',i,dot_product(Vec(:,i),r)
+      END DO
+      CALL dealloc_NParray(Hsave,'Hsave',name_sub)
+      CALL dealloc_NParray(r,'r',name_sub)
+    END IF
+  ENDIF !for MPI_id==0!
 
-!=====================================================================
-!
-!
-!=====================================================================
 !------------------------------------------------------
   IF (debug) THEN
 
     write(out_unitp,*) ' level energy :'
     DO i=1,n
-
       write(out_unitp,*) i,E(i)*auTOcm_inv,(E(i)-E(1))*auTOcm_inv
-
     END DO
 
     write(out_unitp,*) ' Vec:'
@@ -128,8 +126,8 @@
   END IF
 !------------------------------------------------------
 
-
 END SUBROUTINE sub_diago_H
+!=======================================================================================
 
 !
 !=====================================================================
